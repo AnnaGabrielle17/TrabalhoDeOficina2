3a949c8593edb22d9b0925b7fcade82f8b2a6c87
AnnaGabrielle17 <145567496+AnnaGabrielle17@users.noreply.github.com>
2024-09-16 07:43:11 -0300

    parte1

---
+++ TrabalhoOficina/Assets/Pixel Adventure 1/Assets/Scriptes/EfeitoDigitador.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EfeitoDigitador : MonoBehaviour
+{
+    private TextMeshProUGUI componenteTexto;
+    private AudioSource _audioSource;
+    private string mensagemOriginal;
+}
